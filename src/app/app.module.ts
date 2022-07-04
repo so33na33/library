9b262cb8c633ad37bae02ebabbb64373cee947e5
so33na33 <santhoshsona150@gmail.com>
2022-07-04 17:46:47 +0530

    searched successfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddbooksComponent } from './addbooks/addbooks.component';
+import { SearchBookComponent } from './search-book/search-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddbooksComponent
+    AddbooksComponent,
+    SearchBookComponent
   ],
   imports: [
     BrowserModule,
